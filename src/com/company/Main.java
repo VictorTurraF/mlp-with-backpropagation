97538e6b86ca5998db8e67e0776f968ffb95205e
Victor Turra <victor.florencio@univem.edu.br>
2021-06-03 20:31:44 -0300

    Add dev group informations

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,18 @@ package com.company;
 
 import java.util.ArrayList;
 
+/**
+ * Rede neural (Neural Network)
+ * perceptron de multicamadas (MLP - Multi Layer Perceptron)
+ *
+ * Feito por:
+ *
+ * Gabriel Dutra Nascimento RA: 578266
+ * Samuel dos Santos Silva RA: 577588
+ * Victor Turra Florêncio RA: 580899
+ * Vinicius Lucas dos Santos RA: 580694
+ *
+ */
 public class Main {
     public static void main(String[] args) {
 
