ba09eed55291d31fa3f9b9b28ffa47aeecc1417e
Victor Turra <victor.florencio@univem.edu.br>
2021-06-03 20:21:35 -0300

    Create thread to training the neural network

--- src/com/company/NeuralNetworkTraining.java
+++ src/com/company/NeuralNetworkTraining.java
@@ -1,4 +1,16 @@
 package com.company;
 
-public class NeuralNetworkTraining {
+public class NeuralNetworkTraining implements Runnable {
+    public MultiLayerPerceptron multiLayerPerceptron;
+
+    public NeuralNetworkTraining(MultiLayerPerceptron b) {
+        multiLayerPerceptron = b;
+    }
+
+    @Override
+    public void run() {
+        System.out.print(NeuralNetworkReporting.ANSI_BLUE);
+        System.out.println("Iniciando treino [" + multiLayerPerceptron.name + "] ...");
+        multiLayerPerceptron.trainNeuralNetwork(0.45, 0.2);
+    }
 }
